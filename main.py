78395438f348ebfcfe90fd22b9331b57e874f671
Soham-global <magiciancoder1@gmail.com>
2026-02-11 12:50:57 +0530

    Training done and added !

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ChestDiseaseClassifier import logger
 from ChestDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from ChestDiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from ChestDiseaseClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -19,6 +20,18 @@ try:
     logger.info(f"**********")
     logger.info(f">>>>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<<<<")
     obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<<<\n\nx==================x")
+except Exception as e :
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f"**********")
+    logger.info(f">>>>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<<<<")
+    obj = ModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<<<\n\nx==================x")
 except Exception as e :
