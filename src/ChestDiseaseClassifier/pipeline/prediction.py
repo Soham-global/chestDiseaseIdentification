a161f61690d739037ec6756786fd98c281245a75
Soham-global <magiciancoder1@gmail.com>
2026-02-12 12:32:53 +0530

    Interface completed properly

--- src/ChestDiseaseClassifier/pipeline/prediction.py
+++ src/ChestDiseaseClassifier/pipeline/prediction.py
@@ -3,30 +3,23 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import os
 
-
-
 class PredictionPipeline:
-    def __init__(self,filename):
-        self.filename =filename
+    def __init__(self, filename):
+        self.filename = filename
+        # Load model once when the class is created
+        self.model = load_model(os.path.join("model", "model.h5"), compile=False)
+        self.classes = ["Normal", "Adenocarcinoma Cancer"]
 
-
-    
     def predict(self):
-        ## load model
-        
-        # model = load_model(os.path.join("artifacts","training", "model.h5"))
-        model = load_model(os.path.join("model", "model.h5"))
-
-        imagename = self.filename
-        test_image = image.load_img(imagename, target_size = (224,224))
+        # Load and preprocess image
+        test_image = image.load_img(self.filename, target_size=(224, 224))
         test_image = image.img_to_array(test_image)
-        test_image = np.expand_dims(test_image, axis = 0)
-        result = np.argmax(model.predict(test_image), axis=1)
-        print(result)
+        test_image = np.expand_dims(test_image, axis=0)
+        test_image = test_image / 255.0  # normalize
 
-        if result[0] == 1:
-            prediction = 'Normal'
-            return [{ "image" : prediction}]
-        else:
-            prediction = 'Adenocarcinoma Cancer'
-            return [{ "image" : prediction}]
+        # Predict
+        result = np.argmax(self.model.predict(test_image), axis=1)
+        prediction = self.classes[result[0]]
+
+        # Return in dict format
+        return [{"image": prediction}]
